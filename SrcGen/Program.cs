b422c814c3d8e5bba9ed474e99d89faf6e805bae
Paul Chen <lancevdance@gmail.com>
2025-11-05 15:53:38 +0800

    Add tests for contants.

--- SrcGen/Program.cs
+++ SrcGen/Program.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace SrcGen
 {
--- SrcGenTests/StructTests.cs
+++ SrcGenTests/StructTests.cs
@@ -14,7 +14,7 @@ public class StructTests : TestsBase
                 public ULONG64  Size;
             }
             """,
-            """
+            hppSrc: """
             typedef struct _DEBUG_OFFSET_REGION
             {
                 ULONG64 Base;
@@ -23,4 +23,61 @@ public class StructTests : TestsBase
             """,
             "");
     }
+
+    [Fact]
+    public void TestStruct2()
+    {
+        AssertGenerated("""
+            namespace Interop.DbgEng;
+
+            public struct DebugOffsetRegion
+            {
+                public ULONG64  Base;
+                public ULONG64  Size;
+            }
+            """,
+            "",
+            missingSrc: """
+            typedef struct _DEBUG_OFFSET_REGION
+            {
+                ULONG64 Base;
+                ULONG64 Size;
+            } DEBUG_OFFSET_REGION, *PDEBUG_OFFSET_REGION;
+            """);
+    }
+
+    [Fact]
+    public void TestConstant1()
+    {
+        AssertGenerated("""
+            namespace Interop.DbgEng;
+
+            public static partial class Constants
+            {
+                public const UINT32 X = 0;
+            }
+            """,
+            hppSrc: """
+            #define X 0
+            """,
+            "");
+    }
+
+    [Fact]
+    public void TestConstant2()
+    {
+        AssertGenerated("""
+            namespace Interop.DbgEng;
+
+            public static partial class Constants
+            {
+                public const UINT32 X = 0;
+            }
+            """,
+            "",
+            missingSrc: """
+            #define X 0
+            """);
+    }
+
 }
